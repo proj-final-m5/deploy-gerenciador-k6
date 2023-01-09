2c2ba46607604aab8f5392e9dcdcc9a7e9565c4d
Makson <maksonsantoss4@gmail.com>
2023-01-09 18:30:49 -0300

    adicionado configurações de deploy

---
+++ invites/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Invite(models.Model):
+    class Meta:
+        ordering = ["id"]
+
+    is_accept = models.BooleanField(default=False)
+
+    contact = models.ForeignKey(
+        "contacts.Contact",
+        on_delete=models.CASCADE,
+        related_name="invites",
+    )
+
+    task = models.ForeignKey(
+        "tasks.Task",
+        on_delete=models.CASCADE,
+        related_name="invites",
+    )
