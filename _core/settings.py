7307820e25d550cf1e9cf9aaff144f0035204342
Makson <maksonsantoss4@gmail.com>
2023-01-09 19:12:35 -0300

    adicionado allowed_hosts

--- _core/settings.py
+++ _core/settings.py
@@ -32,7 +32,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "deploy-gerenciador-k6-production.up.railway.app",
+    "0.0.0.0",
+]
 
 
 # Application definition
